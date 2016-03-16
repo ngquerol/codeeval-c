b159efa93b18a6918e940163c08cb6ad6584ce07
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2016-03-16 02:09:17 +0100

    big_digits.c: Minor fixes (derp)

--- big_digits/big_digits.c
+++ big_digits/big_digits.c
@@ -2,6 +2,76 @@
 
 int main(int argc, char** argv) {
 
+    const char *zero[][1]  = {{"-**--"},
+                              {"*--*-"},
+                              {"*--*-"},
+                              {"*--*-"},
+                              {"-**--"},
+                              {"-----"}};
+
+    const char *one[][1]   = {{"--*--"},
+                              {"-**--"},
+                              {"--*--"},
+                              {"--*--"},
+                              {"-***-"},
+                              {"-----"}};
+
+    const char *two[][1]   = {{"***--"},
+                              {"---*-"},
+                              {"-**--"},
+                              {"*----"},
+                              {"****-"},
+                              {"-----"}};
+
+    const char *three[][1] = {{"***--"},
+                              {"---*-"},
+                              {"-**--"},
+                              {"---*-"},
+                              {"***--"},
+                              {"-----"}};
+
+    const char *four[][1]  = {{"-*---"},
+                              {"*--*-"},
+                              {"****-"},
+                              {"---*-"},
+                              {"---*-"},
+                              {"-----"}};
+
+    const char *five[][1]  = {{"****-"},
+                              {"*----"},
+                              {"***--"},
+                              {"---*-"},
+                              {"***--"},
+                              {"-----"}};
+
+    const char *six[][1]   = {{"-**--"},
+                              {"*----"},
+                              {"***--"},
+                              {"*--*-"},
+                              {"-**--"},
+                              {"-----"}};
+
+    const char *seven[][1] = {{"****-"},
+                              {"---*-"},
+                              {"--*--"},
+                              {"-*---"},
+                              {"-*---"},
+                              {"-----"}};
+
+    const char *eight[][1] = {{"-**--"},
+                              {"*--*-"},
+                              {"-**--"},
+                              {"*--*-"},
+                              {"-**--"},
+                              {"-----"}};
+
+    const char *nine[][1]  = {{"-**--"},
+                              {"*--*-"},
+                              {"-***-"},
+                              {"---*-"},
+                              {"-**--"},
+                              {"-----"}};
+
     FILE* file;
     char line[80];
 
@@ -17,126 +87,57 @@ int main(int argc, char** argv) {
 
     while (fgets(line, sizeof line, file)) {
 
-        const char *zero[][1]  = {{"-**--"},
-                                  {"*--*-"},
-                                  {"*--*-"},
-                                  {"*--*-"},
-                                  {"-**--"},
-                                  {"-----"}};
-
-        const char *one[][1]   = {{"--*--"},
-                                  {"-**--"},
-                                  {"--*--"},
-                                  {"--*--"},
-                                  {"-***-"},
-                                  {"-----"}};
-
-        const char *two[][1]   = {{"***--"},
-                                  {"---*-"},
-                                  {"-**--"},
-                                  {"*----"},
-                                  {"****-"},
-                                  {"-----"}};
-
-        const char *three[][1] = {{"***--"},
-                                  {"---*-"},
-                                  {"-**--"},
-                                  {"---*-"},
-                                  {"***--"},
-                                  {"-----"}};
-
-        const char *four[][1]  = {{"-*---"},
-                                  {"*--*-"},
-                                  {"****-"},
-                                  {"---*-"},
-                                  {"---*-"},
-                                  {"-----"}};
-
-        const char *five[][1]  = {{"****-"},
-                                  {"*----"},
-                                  {"***--"},
-                                  {"---*-"},
-                                  {"***--"},
-                                  {"-----"}};
-
-        const char *six[][1]   = {{"-**--"},
-                                  {"*----"},
-                                  {"***--"},
-                                  {"*--*-"},
-                                  {"-**--"},
-                                  {"-----"}};
-
-        const char *seven[][1] = {{"****-"},
-                                  {"---*-"},
-                                  {"--*--"},
-                                  {"-*---"},
-                                  {"-*---"},
-                                  {"-----"}};
-
-        const char *eight[][1] = {{"-**--"},
-                                  {"*--*-"},
-                                  {"-**--"},
-                                  {"*--*-"},
-                                  {"-**--"},
-                                  {"-----"}};
-
-        const char *nine[][1]  = {{"-**--"},
-                                  {"*--*-"},
-                                  {"-***-"},
-                                  {"---*-"},
-                                  {"-**--"},
-                                  {"-----"}};
-
         for (int i = 0; i < 6; i++) {
 
             char *p = line;
 
             while (*p) {
-                // convert the char to an int (basically, remove the NUL
-                // character directly following it)
+                // convert the char to an int by calculating the difference between
+                // its ASCII code and the one for 0. (e.g. '5' = 53, '0' = 48 ->
+                // '5' - '0' == 53 - 48 == 5)
                 switch (*p - '0') {
-                    case 0:
-                        printf("%s", zero[i][0]);
-                        break;
+                case 0:
+                    printf("%s", zero[i][0]);
+                    break;
 
-                    case 1:
-                        printf("%s", one[i][0]);
-                        break;
+                case 1:
+                    printf("%s", one[i][0]);
+                    break;
 
-                    case 2:
-                        printf("%s", two[i][0]);
-                        break;
+                case 2:
+                    printf("%s", two[i][0]);
+                    break;
 
-                    case 3:
-                        printf("%s", three[i][0]);
-                        break;
+                case 3:
+                    printf("%s", three[i][0]);
+                    break;
 
-                    case 4:
-                        printf("%s", four[i][0]);
-                        break;
+                case 4:
+                    printf("%s", four[i][0]);
+                    break;
 
-                    case 5:
-                        printf("%s", five[i][0]);
-                        break;
+                case 5:
+                    printf("%s", five[i][0]);
+                    break;
 
-                    case 6:
-                        printf("%s", six[i][0]);
-                        break;
+                case 6:
+                    printf("%s", six[i][0]);
+                    break;
 
-                    case 7:
-                        printf("%s", seven[i][0]);
-                        break;
+                case 7:
+                    printf("%s", seven[i][0]);
+                    break;
 
-                    case 8:
-                        printf("%s", eight[i][0]);
-                        break;
+                case 8:
+                    printf("%s", eight[i][0]);
+                    break;
 
-                    case 9:
-                        printf("%s", nine[i][0]);
-                        break;
+                case 9:
+                    printf("%s", nine[i][0]);
+                    break;
 
-                    default:
-                        break;
+                default:
+                    break;
                 }
                 p++;
             }
